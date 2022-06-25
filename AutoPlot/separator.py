407b840ddf9461da810a4e3846129f8216eded7d
gripani <g.ripani93@gmail.com>
2022-06-25 17:15:57 +0200

    Change y to fnct in separator

--- AutoPlot/separator.py
+++ AutoPlot/separator.py
@@ -1,17 +1,17 @@
 class Separator:
-    def __init__(self, y, style, label):
-        self.y = y 
+    def __init__(self, fnct, style, label):
+        self.fnct = fnct 
         self.style = style 
         self.label = label  
     
     def separate(self):
-        if ';' in self.y:
-            self.y = self.y.split(';')
+        if ';' in self.fnct:
+            self.fnct = self.fnct.split(';')
             self.style = self.style.split(';')
             self.label = self.label.split(';')
-            assert len(self.y) == len(self.style) == len(self.label)
+            assert len(self.fnct) == len(self.style) == len(self.label)
         else:
-            self.y = [self.y]
+            self.fnct = [self.fnct]
             self.style = [self.style]
             self.label = [self.label]
-        return self.y, self.style, self.label  
+        return self.fnct, self.style, self.label  

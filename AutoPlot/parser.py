aa0a9f301a0d3cbb9a362296d9c69b920edd398f
gripani <g.ripani93@gmail.com>
2022-06-26 12:30:15 +0200

    Added help in parser

--- AutoPlot/parser.py
+++ AutoPlot/parser.py
@@ -3,14 +3,14 @@ import argparse
 class Parser:
     def __init__(self):
         self.parser = argparse.ArgumentParser()
-        self.parser.add_argument('--fnct', type=str)
-        self.parser.add_argument('--x0', type=str)
-        self.parser.add_argument('--x1', type=str)
-        self.parser.add_argument('--style', type=str)
-        self.parser.add_argument('--plt_label', type=str)
-        self.parser.add_argument('--x_label', type=str)
-        self.parser.add_argument('--y_label', type=str)
-        self.parser.add_argument('--plt_title', type=str)
+        self.parser.add_argument('--fnct', type=str, help='inline function of x to plot, separate multiple values with semicolumn')
+        self.parser.add_argument('--x0', type=str, help='lower limit of x-axis')
+        self.parser.add_argument('--x1', type=str, help='upper limit of x-axis')
+        self.parser.add_argument('--style', type=str, help='linestyle for plot function, separate multiple values with semicolumn')
+        self.parser.add_argument('--plt_label', type=str, help='label for plot function, separate multiple values with semicolumn')
+        self.parser.add_argument('--x_label', type=str, help='label for x-axis')
+        self.parser.add_argument('--y_label', type=str, help='label for y-axis')
+        self.parser.add_argument('--plt_title', type=str, help='plot title')
     
     def get_args(self):
         args = self.parser.parse_args()

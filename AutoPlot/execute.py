3a9a092540bbaa3e2a9283d62f35738bdd4c5653
gripani <g.ripani93@gmail.com>
2022-06-27 15:24:17 +0200

    creating my_exec function

--- AutoPlot/execute.py
+++ AutoPlot/execute.py
@@ -1,4 +1,28 @@
+import sys 
+import traceback
+
 from .numpy_strings import numpyize 
+
+
+class InterpreterError(Exception):
+
+    pass 
+
+def my_exec(src, globals, locals, description):
+    try:
+        exec(src, globals, locals)
+    except SyntaxError as err:
+        error_class = err.__class__.__name__
+        detail = err.args[0]
+        line_number = err.lineno
+    except Exception as err:
+        error_class = err.__class__.__name__
+        detail = err.args[0]
+        cl, exc, tb = sys.exc_info()
+        line_number = traceback.extract_tb(tb)[-1][1]
+    else:
+        return
+    raise InterpreterError(f"{error_class} at line {line_number} of {description}: {detail}" % (error_class, line_number, description, detail))
 
 class Execute:
 
@@ -21,6 +45,6 @@ class Execute:
             self.x1 = numpyize(self.x1) 
         src = f"xi = np.linspace({self.x0}, {self.x1}, {self.n}); yi = np.array([{self.funct} for x in xi])"
         import numpy as np 
-        exec(src, {'np': np}, kw)  
+        my_exec(src=src, globals={'np': np}, locals=kw, description='apply numpy call of x and y')
         x, y = kw['xi'], kw['yi']
         return x, y 

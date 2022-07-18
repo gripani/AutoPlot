ed09373c44aa7e826442d33531db9ee0eba63954
gripani <g.ripani93@gmail.com>
2022-07-18 10:38:27 +0200

    Modified WebApp templating logic

--- WebApp/my_server.py
+++ WebApp/my_server.py
@@ -1,10 +1,16 @@
 import sys
+import os.path as osp 
+from pathlib import Path 
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 HOST_NAME = "localhost"
 PORT = 8080
+DIRECTORY = osp.join(Path(__file__).parent, "templates") 
 
 class PythonServer(SimpleHTTPRequestHandler):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=DIRECTORY, **kwargs)
 
     def end_headers(self):
         self.send_header('Access-Control-Allow-Origin', '*')
